3b7c868f362c2f2c4a9fbc4bbad649ecd33d0ec2
Adam <agillfi@ncsu.edu>
2014-03-29 18:10:03 -0400

    changed name of app

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mental_health_app',
+    'mental',
 )
 
 MIDDLEWARE_CLASSES = (
