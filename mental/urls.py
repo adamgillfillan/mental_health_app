1f7c062cdb76468c74da71a855e63ee70931d902
Adam <agillfi@ncsu.edu>
2014-03-29 19:35:36 -0400

    added users view

--- mental/urls.py
+++ mental/urls.py
@@ -6,4 +6,5 @@ from mental import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
+    url(r'^users', views.users, name='users'),
 )
--- mental/views.py
+++ mental/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from mental.models import UserProfile
 
 
 # Create your views here.
@@ -8,3 +9,17 @@ def index(request):
     context = RequestContext(request)
     context_dict = {}
     return render_to_response('mental/index.html', context_dict, context)
+
+
+def users(request):
+    context = RequestContext(request)
+
+    user_list = get_user_list()
+
+    context_dict = {'user_list': user_list}
+    return render_to_response('mental/users.html', context_dict, context)
+
+
+def get_user_list():
+    user_list = UserProfile.objects.all()
+    return user_list
