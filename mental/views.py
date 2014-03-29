420dda8de59f6f2be46c0c3b3d5d5460dc29ec88
Adam <agillfi@ncsu.edu>
2014-03-29 18:19:25 -0400

    index view working

--- mental/views.py
+++ mental/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render_to_response
 def index(request):
     context = RequestContext(request)
     context_dict = {}
-    return render_to_response('nsa/index.html', context_dict, context)
+    return render_to_response('mental/index.html', context_dict, context)
