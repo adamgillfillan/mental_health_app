6830e34673687ba1d7c850e9fadc3546bbe70f43
Adam <agillfi@ncsu.edu>
2014-03-30 11:14:34 -0400

    added populate user

---
+++ populate_user.py
@@ -0,0 +1,25 @@
+__author__ = 'Adam'
+import os
+import random
+
+
+def add_user(name, email):
+    user = UserProfile.objects.get_or_create(name=name, email=email)[0]
+
+    return user
+
+
+def populate():
+    add_user("Collin", "cyborg@ut.edu")
+    # Print out all users
+    for user in UserProfile.objects.all():
+        print("Name: {0}, Email: {1}".format(
+            user.name,
+            user.email,
+        ))
+# Start execution here!
+if __name__ == '__main__':
+    print("Starting mental health population script...")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mental_health.settings')
+    from mental.models import UserProfile, Voltage
+    populate()
