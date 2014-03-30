e567dc204606ba518637f0f52cdabee3b659def7
Adam <agillfi@ncsu.edu>
2014-03-29 23:42:28 -0400

    updated client

--- mental/client.py
+++ mental/client.py
@@ -7,7 +7,7 @@ udp_host = socket.gethostname()
 udp_port = 3001
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((udp_ip, udp_port))
+sock.bind((udp_host, udp_port))
 
 while 1:
     data, addr = sock.recvfrom(1024)
