0ca7f5b4c64cc3175e49189b149f38fecdb47473
Adam <agillfi@ncsu.edu>
2014-03-30 07:43:06 -0400

    adding chart

--- mental/views.py
+++ mental/views.py
@@ -48,7 +48,8 @@ def addvoltages(request):
         v = Voltage(voltage = request.POST['voltages'], user = get_user_list()[0])
         v.save()
     return HttpResponse('Data added')
-        
+
+
 def user(request, user_name_url):
     context = RequestContext(request)
 
@@ -65,5 +66,41 @@ def user(request, user_name_url):
     context_dict['voltages'] = voltages_list
     context_dict['user'] = my_user
 
+
+    # begin charting
+    xdata = voltages_list.values('voltage')
+    #ydata = voltages_list.values('time')
+    ydata = voltages_list.values('user')
+    chartdata = {'x': xdata,
+                 'name1': user_name, 'y1': ydata}
+    charttype = "lineChart"
+    chartcontainer = 'linechart_container'
+    data = {
+        'charttype': charttype,
+        'chartdata': chartdata,
+        'chartcontainer': chartcontainer,
+        'extra': {'x_is_date': False}
+    }
+    context_dict['data'] = data
     # Go render the response and return it to the client.
     return render_to_response('mental/user.html', context_dict, context)
+
+# working code:
+# def user(request, user_name_url):
+#     context = RequestContext(request)
+#
+#     user_name = user_name_url
+#     context_dict = {'user_name': user_name,
+#                     'user_name_url': user_name_url}
+#     user_list = get_user_list()
+#     context_dict['user_list'] = user_list
+#
+#     my_user = UserProfile.objects.get(name=user_name)
+#     voltages = Voltage.objects.filter(user=my_user)
+#     voltages_list = voltages
+#
+#     context_dict['voltages'] = voltages_list
+#     context_dict['user'] = my_user
+#
+#     # Go render the response and return it to the client.
+#     return render_to_response('mental/user.html', context_dict, context)
--- mental_health/settings.py
+++ mental_health/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mental',
+    'django_nvd3'
 )
 
 MIDDLEWARE_CLASSES = (
