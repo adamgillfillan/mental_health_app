27018672460198feb6d99cb68679943e8f6b9bee
Adam <agillfi@ncsu.edu>
2014-03-29 23:38:01 -0400

    added client

---
+++ mental/client.py
@@ -0,0 +1,14 @@
+__author__ = 'Adam'
+
+import socket
+s = socket.socket()
+
+udp_host = socket.gethostname()
+udp_port = 3001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((udp_ip, udp_port))
+
+while 1:
+    data, addr = sock.recvfrom(1024)
+    print("Message: ", data)
